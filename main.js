9725b0156b8c6a76f3e84f0a6f5cc62afa3a4dd4
Kevin Poppe <kevin.poppe93@gmail.com>
2022-02-13 14:17:07 +0100

    Made first Layout. Added images. Added some Content to main-section. Made simple animations and hover-effects

---
+++ main.js
@@ -0,0 +1,179 @@
+/************************/
+/*      Patienten       */
+/************************/
+
+let patientenAkten = {
+
+    a : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    b : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    c : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    d : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    e : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    f : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    g : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    h : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    },
+    i : {
+        Vorname : "Berta",
+        Nachmane : "Mustermann",
+        Geburtsdatum : "01.01.1950",
+        Wohnort : "Walldürn",
+        Strasse : "Musterstraße",
+        Hausnummer : "1a",
+        Telefon : "01234/012012",
+        Angehoerige : [["Tochter" , "Anna" , "Mustermann" , "01234/012012", "0160/01234567", "01234 Buxdehude"], ["Sohn", "Alfred", "Müller", "01234/012012", "0160/01234567", "01234 Buxdehude"]],
+        Versicherung : "AOK Baden-Württemberg",
+        Hauswirtschaft : true,
+        Pflege : true,
+        Beschaeftigung : false,
+        Bezugspflegekraft : "Andrea Müller",
+        Notruf : true,
+        Dement : false,
+        Notiz : "Eingang um die Ecke"
+    }
+
+}
+
+console.log(patientenAkten.a)
+
+/*let test = document.createElement("div");
+test.className = "eins";
+
+document.body.appendChild(test);*/
+
+
